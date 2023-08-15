16785059002866340e9eaa6d666a36d086131c4d
Ramiro Ceballos <screen_toto@hotmail.com>
2023-08-15 12:17:29 -0300

    configure docker-compose and program, first migration and implementation.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+
+builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(connectionString));
+
+//Esto se usa para sacar el string connection de appsettings.json
+//builder.Services.AddDbContext<MyContext>(options => 
+//options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnectionNet6"))); 
 
 var app = builder.Build();
 
@@ -16,8 +30,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+PrepareDb.Population(app);
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
