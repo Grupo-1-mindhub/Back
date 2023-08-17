81c58dfa4d92ec934453c46c6dd42fa732a80aae
MartinaAngelico <85707736+MartinaAngelico@users.noreply.github.com>
2023-08-17 11:59:17 -0300

    inyectando dependecnia del contexto y creacion de repobase

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,7 @@
 using backend.Models;
+using backend.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +18,27 @@ var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
 var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
 
 builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(connectionString));
-
+//builder.Services.AddScoped<IRepositoryBase, RepositoryBase>();
 //Esto se usa para sacar el string connection de appsettings.json
 //builder.Services.AddDbContext<MyContext>(options => 
 //options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnectionNet6"))); 
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+	try
+	{
+		var context = services.GetRequiredService<MyContext>();
+	}
+	catch (Exception ex)
+	{
+		var logger = services.GetRequiredService<ILogger<Program>>();
+		logger.LogError(ex, "Ha ocurrido un error al enviar la info a la base de datos.");
+	}
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
