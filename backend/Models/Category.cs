c999b48ead62aa4ff8d5ed61c23fbcb85db98b59
Ramiro Ceballos <screen_toto@hotmail.com>
2023-08-18 12:11:59 -0300

    modelos

---
+++ backend/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Budget>? Budgets { get; set; }
+    }
+}
---
+++ backend/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public double Amount { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Budget? Budget { get; set; }
+        public long BudgetId { get; set; }
+        public PaymentMethod? PaymentMethod { get; set; }
+        public long PaymentMethodId { get; set; }
+    }
+}
