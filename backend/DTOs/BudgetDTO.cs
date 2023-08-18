dbba29cb11e7efc0da2dccab2821eb03c06629e3
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2023-08-18 12:17:21 -0300

    Creo los dto

---
+++ backend/DTOs/BudgetDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.DTOs
+{
+    public class BudgetDTO
+    {
+        public long Id { get; set; }
+
+        public double Balance { get; set; }
+
+        public ICollection<AccountDTO> Accounts { get; set; }
+  
+    }
+}
---
+++ backend/DTOs/ClientDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.DTOs
+{
+    public class ClientDTO
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<AccountDTO> Accounts { get; set; }
+    }
+}
