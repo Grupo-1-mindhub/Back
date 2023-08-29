8a10ad1beba774f5791dae81746cecc6daf5c989
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2023-08-29 12:02:00 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- backend/Controllers/StatisticsController.cs
+++ backend/Controllers/StatisticsController.cs
@@ -16,77 +16,42 @@ namespace backend.Controllers
    
             _clientRepository = clientRepository;
         }
-       // [HttpGet("clients/accounts/{id}/statistics")]
-        //public IActionResult GetBudgetsByAccount(long id)
-        //{
-        //    try
-        //    {
+        [HttpGet("clients/accounts/{id}/statistics")]
+        public IActionResult GetBudgetsByAccount(long id)
+        {
+            try
+            {
 
-        //        //Client cl = _clientRepository.FindById(id);
-        //        //if (cl == null)
-        //        //{
-        //        //    return StatusCode(403, "Cliente no encontrado");
-        //        //}
+                Client cl = _clientRepository.FindById(id);
+                if (cl == null)
+                {
+                    return StatusCode(403, "Cliente no encontrado");
+                }
 
-        //        //var accs = cl.Accounts;
-        //        //if (accs== null)
-        //        //    return StatusCode(403, "El cliente no tiene cuentas");
-
-        //        //var buds = new List<Budget>();
-                
-        //        //foreach (Account acc in accs)
-        //        //{
-
-        //        //    foreach (Budget budget in acc.Budgets) 
-        //        //    { 
-        //        //        buds.Add(budget);
-        //        //    }
-        //        //}
-
-        //        //    var allTransactions = new List<TransactionDTO>();
-
-        //        //foreach (Budget bud in budgets)
-        //        //{
-        //        //    allTransactions.AddRange((IEnumerable<TransactionDTO>)bud.Transactions);
-        //        //}
-        //        ////var transactionsByMonthAndYear = allTransactions
-        //        //            .GroupBy(tr => new { tr.CreationDate.Year, tr.CreationDate.Month })
-        //        //            .ToDictionary(
-        //        //                group => $"{group.Key.Year}-{group.Key.Month}",
-        //        //                group => group.Select(tr => new TransactionDTO
-        //        //                {
-        //        //                    Id = tr.Id,
-        //        //                    Amount = tr.Amount,
-        //        //                    Description = tr.Description,
-        //        //                    CreationDate = tr.CreationDate
-        //        //                }).ToList()
-        //        //            );
-
-        //        //var monthsWithTransactions = transactionsByMonthAndYear.Keys.Select(key =>
-        //        //{
-        //        //    var parts = key.Split('-');
-        //        //    return new MonthDTO
-        //        //    {
-        //        //        Year = int.Parse(parts[0]),
-        //        //        Month = (MonthsType)int.Parse(parts[1]),
-        //        //        Amount = transactionsByMonthAndYear[key].Sum(tr => tr.Amount)
-        //        //    };
-        //        //}).ToList();
+                var accs = cl.Accounts;
+                if (accs == null)
+                    return StatusCode(403, "El cliente no tiene cuentas");
 
 
 
-        //        //return Ok(monthsWithTransactions);
+                var trans = new List<Transaction>();
+                foreach (Account acc in accs)
+                {
+                    foreach (Transaction trs in acc.Transactions)
+                    {
+                        trans.Add(trs);
+                    }
+                    
+                }
 
+        
+                return Ok(trans);
+            }
 
-
-        //        //  return Ok(allTransactions);
-        //    //    return Ok(buds);
-        //    }
-
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, ex.Message);
-        //    }
-        //}
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
